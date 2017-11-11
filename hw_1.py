68cf647d18fbd500c2000f4a6d7b877d26ab3d27
Bartek <bartektao@airu.com.hk>
2017-11-11 23:00:16 +0800

    灰階圖

--- hw_1.py
+++ hw_1.py
@@ -1,8 +1,27 @@
 from PIL import Image
-import matplotlib.pyplot as plt
 
-image_1 = Image.open("hw_1_1.jpg")
+image_in = Image.open("hw_1_1.jpg")
 
-plt.figure("image_1")
-plt.imshow(image_1)
-plt.show()
+print(image_in.size)
+
+print(image_in.getpixel((0,0)))
+print(image_in.getpixel((0,1)))
+
+image_temp1 = image_in.point(lambda i : i*(1/3))
+
+
+print(image_temp1.getpixel((0,0)))
+print(image_temp1.getpixel((0,1)))
+
+m_gray = Image.new("L", image_in.size)
+
+for y in range(image_in.size[1]):
+    for x in range(image_in.size[0]):
+        count = image_temp1.getpixel((x,y))
+        sum = count[0]+count[1]+count[2]
+        m_gray.putpixel((x,y),sum)
+m_gray.save("gray.jpg")
+m_gray.show()
+
+
+
