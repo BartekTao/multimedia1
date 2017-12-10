e74e75cb928e17ed8e63d4ca77b6b4079a3e6356
Bartek <bartektao@airu.com.hk>
2017-12-10 23:40:19 +0800

    第一小節 節拍、休息間隔優化

--- hw_2.py
+++ hw_2.py
@@ -12,10 +12,12 @@ except Exception as e:
     print("路徑錯誤")'''
 #不使用讀取txt功能，因為音樂有一拍、半拍、三連音...等等，txt較難統一、辨別以上所述的分類
 ############
+sleep = 30
+time = 0.4
 
 volume = 0.5     # range [0.0, 1.0]
 fs = 8000      # sampling rate, Hz, must be integer
-duration = 4.0   # in seconds, may be float
+duration = 4.0*time   # in seconds, may be float
 #取聖誕歌前四個音節
 #|333-|333-|3212|3---|
 
@@ -27,29 +29,27 @@ f3 = 294.0
 f0 = 1.0
 
 samples = 2*np.pi*np.arange(fs*duration)
-temp = samples
-print(temp)
 
-sleep = 10
 
-n = 1
-for i in range((n-1)*fs,fs*n):
+
+n = 1*time
+for i in range(int((n-time)*fs),int(fs*n)):
     samples[i] = np.sin(samples[i] * f3 / fs).astype(np.float32)
-for i in range(int(fs*n-fs/sleep),fs*n):
+for i in range(int(fs*n-fs/sleep),int(fs*n)):
     samples[i] = np.sin(samples[i] * f0 / fs).astype(np.float32)
 
-n = 2
-for i in range((n-1)*fs,fs*n):
+n = 2*time
+for i in range(int((n-time)*fs),int(fs*n)):
     samples[i] = np.sin(samples[i] * f3 / fs).astype(np.float32)
-for i in range(int(fs*n-fs/sleep),fs*n):
+for i in range(int(fs*n-fs/sleep),int(fs*n)):
     samples[i] = np.sin(samples[i] * f0 / fs).astype(np.float32)
 
-n = 3
-for i in range((n-1)*fs,fs*n):
+n = 3*time
+for i in range(int((n-time)*fs),int(fs*n)):
     samples[i] = np.sin(samples[i] * f3 / fs).astype(np.float32)
 
-n = 4
-for i in range((n-1)*fs,fs*n):
+n = 4*time
+for i in range(int((n-time)*fs),int(fs*n)):
     samples[i] = np.sin(samples[i] * f3 / fs).astype(np.float32)
 
 
