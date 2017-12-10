dc20d54e3020e3f93e847dfef0152195df5f4852
Bartek <bartektao@airu.com.hk>
2017-12-10 22:03:46 +0800

    更改 不使用讀取txt

--- hw_2.py
+++ hw_2.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #讀txt
-path = input('請輸入文字檔路徑+檔名+副檔名：')
+'''path = input('請輸入文字檔路徑+檔名+副檔名：')
 
 try:
     fileTxt = open(path)
 except Exception as e:
-    print("路徑錯誤")
+    print("路徑錯誤")'''
+#不使用讀取txt功能，因為音樂有一拍、半拍、三連音...等等，txt較難統一、辨別以上所述的分類
+############
+
