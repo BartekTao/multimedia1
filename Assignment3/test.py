0b206384e9ae597811484d272da3a9117a627f31
Bartek <bartektao@airu.com.hk>
2018-01-25 21:43:17 +0800

    target frame劃分

--- Assignment3/test.py
+++ Assignment3/test.py
@@ -1,10 +1,12 @@
 import numpy as np
 from PIL import Image
+import pdb
 
 
 def as_array(filepath):
     f = open(filepath, 'r')
     w, h = size = tuple(int(v) for v in next(f).split()[1:3])
+    print(w,h)
     data_size = w * h * 2
 
     f.seek(0, 2)
@@ -22,5 +24,19 @@ def as_array(filepath):
 
     return data
 
-a = as_array('i1.pgm')
-print(a)
+a = as_array('i2.pgm')
+np3 = a.astype('uint8')
+
+w = 320
+h = 240
+macroblock = 16
+window = 31
+
+count = 0
+for j in range(int(h /macroblock)):
+    for i in range(int(w / macroblock)):
+        target = np3[i*16:16*(i+1),j*16:16*(j+1)]
+        count = count+1
+        #print(target)
+        #pdb.set_trace()
+print(count)
